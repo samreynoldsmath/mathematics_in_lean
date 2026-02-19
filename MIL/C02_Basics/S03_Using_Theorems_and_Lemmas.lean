d1798fd3011e9939cc2e48011f3aed8f62e71353
Sam Reynolds <sreyn@proton.me>
2026-02-18 16:52:55 -0800

    more of chapter 2

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,9 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  apply lt_of_le_of_lt h₂ h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,21 +88,32 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  rw [exp_le_exp]
+  apply add_le_add_left
+  exact h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    linarith [exp_pos a]
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left (exp_le_exp.mpr h)
 
 example : 0 ≤ a ^ 2 := by
-  -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  -- refine tsub_le_tsub ?_ ?_
+  -- apply? -- does not work for me...
+  apply add_le_add_left
+  -- apply?
+  -- refine neg_le_neg_iff.mpr ?_
+  apply neg_le_neg_iff.mpr
+  apply exp_le_exp.mpr
+  exact h
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -120,8 +133,28 @@ example : 2*a*b ≤ a^2 + b^2 := by
     _ ≥ 0 := by apply pow_two_nonneg
   linarith
 
+lemma lem₁ : a*b ≤ (a^2 + b^2)/2 := by
+  have h : 0 ≤ a^2 - 2*a*b + b^2
+  calc
+    a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  linarith
+
+lemma lem₂ : -a*b ≤ (a^2 + b^2)/2 := by
+  have h : 0 ≤ a^2 + 2*a*b + b^2
+  calc
+    a^2 + 2*a*b + b^2 = (a + b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  linarith
+
 example : |a*b| ≤ (a^2 + b^2)/2 := by
+  apply abs_le'.mpr
+
+  -- apply lem₁
+  -- apply lem₂
+  -- ???????
   sorry
 
-#check abs_le'.mpr
-
+#check abs_le
+#check abs_le'
+#check abs_le'.mpr -- huh??
--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -38,18 +38,105 @@ example : min a b = min b a := by
     apply min_le_right
     apply min_le_left
 
+#check le_max_left
+#check le_max_right
+#check max_le
+
+-- goofy
+lemma lem₁ : max a b ≤ max b a := by
+  apply max_le
+  apply le_max_right
+  apply le_max_left
+
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  apply lem₁
+  apply lem₁
+
+-- recommended, clean
+example : max a b = max b a := by
+  apply le_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+
+-- definitely the most complicated way to prove this
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  -- show both sides are bounded by a,b,c
+  have left_bound_a : min (min a b) c ≤ a:= by
+    apply le_trans
+    apply min_le_left
+    apply min_le_left
+  have left_bound_b : min (min a b) c ≤ b:= by
+    apply le_trans
+    apply min_le_left
+    apply min_le_right
+  have left_bound_c : min (min a b) c ≤ c:= by
+    apply min_le_right
+  have right_bound_a : min a (min b c) ≤ a:= by
+    apply min_le_left
+  have right_bound_b : min a (min b c) ≤ b:= by
+    apply le_trans
+    apply min_le_right
+    apply min_le_left
+  have right_bound_c : min a (min b c) ≤ c:= by
+    apply le_trans
+    apply min_le_right
+    apply min_le_right
+  -- use the inequalities above to prove equality
+  apply le_antisymm
+  . show min (min a b) c ≤ min a (min b c)
+    apply le_min
+    . apply left_bound_a
+    . apply le_min
+      . apply left_bound_b
+      apply left_bound_c
+  . show min a (min b c) ≤ min (min a b) c
+    apply le_min
+    . apply le_min
+      . apply right_bound_a
+      apply right_bound_b
+    . apply right_bound_c
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  . show min a b + c ≤ a + c
+    have h₁ : min a b ≤ a := by apply min_le_left
+    linarith
+  . show min a b + c ≤ b + c
+    have h₂ : min a b ≤ b := by apply min_le_right
+    linarith
+
+theorem aux2 : min (a + c) (b + c) - c ≤ a := by
+  have h : min (a + c) (b + c) ≤ a + c := by
+    apply min_le_left
+  linarith
+
+theorem aux3 : min (a + c) (b + c) - c ≤ b := by
+  have h : min (a + c) (b + c) ≤ b + c := by
+    apply min_le_right
+  linarith
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  . apply aux
+  . show min (a + c) (b + c) ≤ min a b + c
+    have h : min (a + c) (b + c) - c ≤ min a b := by
+      apply le_min
+      . apply aux2 -- super hacky
+      . apply aux3
+    linarith
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  -- not in three lines or less... but neither is the official solution!
+  have h : |a| ≤ |a - b| + |b| := by
+    calc
+      |a| = |(a - b) + b| := by ring
+      _ ≤ |a - b| + |b| := by apply abs_add
+  linarith
 end
 
 section
@@ -65,8 +152,25 @@ example : x ∣ y * x * z := by
 example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
+#check dvd_add
+#check dvd_add_left
+#check dvd_add_right
+#check dvd_pow
+#check dvd_mul_of_dvd_left
+
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  . show x ∣ y * (x * z) + x ^ 2
+    apply dvd_add
+    . show x ∣ y * (x * z)
+      have h₁ : y * (x * z) = (y * z) * x := by ring
+      rw [h₁]
+      apply dvd_mul_left
+    . show x ∣ x ^ 2
+      apply dvd_mul_left
+  . show x ∣ w ^ 2
+    apply dvd_pow h
+    exact Ne.symm (Nat.zero_ne_add_one 1) -- wild way to state 2 ≠ 0
 end
 
 section
@@ -77,8 +181,9 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_right n : Nat.lcm n 0 = 0)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
+#check Nat.gcd_def
+#check Nat.gcd_comm
+
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply Nat.gcd_comm -- cheating?
 end
-
-
